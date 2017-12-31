6611a9ef4342151f62642a12eb8a10a5e152cc55
Mohcine EL KASSIB <elkassib.mohcine@gmail.com>
2017-12-31 02:49:21 +0000

    Add auto increment id

--- client/components/layouts/GameBoard/index.js
+++ client/components/layouts/GameBoard/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
@@ -6,9 +6,9 @@ import CategoryName from 'Components/components/CategoryName';
 import QuestionCounter from 'Components/components/QuestionCounter';
 import Question from 'Components/components/Question';
 import ChoiceList from 'Components/components/ChoiceList';
-import ContinueButton from 'Components/components/ContinueButton';
 import ResultDisplay from 'Components/components/ResultDisplay';
 
+import { setIdNextQuestion } from 'Store/actions/game';
 import { selectQuestion, selectChoices } from 'Store/reducers/game';
 
 const stateToProps = state => ({
@@ -20,39 +20,59 @@ const stateToProps = state => ({
   choices: selectChoices(state)
 });
 
-function GameBoard(props) {
-  const {
-    counter,
-    numberOfQuestions,
-    categoryName,
-    question,
-    choices,
-    hasQuestionAnswered
-  } = props;
+const dispatchToProps = {
+  onSetIdNextQuetion: setIdNextQuestion
+};
 
-  if (counter > numberOfQuestions) {
-    return <ResultDisplay total={numberOfQuestions} />;
+class GameBoard extends Component {
+  componentWillReceiveProps(newProps) {
+    if (newProps.hasQuestionAnswered) {
+      setTimeout(() => {
+        this.props.onSetIdNextQuetion();
+      }, 1500);
+    }
   }
 
-  return (
-    <div className="container has-text-centered">
-      <CategoryName name={categoryName} />
-      <QuestionCounter counter={counter} total={numberOfQuestions} />
-      <Question question={question} />
-      <hr />
-      <ChoiceList choices={choices} />
-      {hasQuestionAnswered && <ContinueButton value="Continue" />}
-    </div>
-  );
+  shouldComponentUpdate(newProps) {
+    const { choices } = this.props;
+    if (JSON.stringify(choices) === JSON.stringify(newProps.choices)) {
+      return false;
+    }
+    return true;
+  }
+
+  render() {
+    const {
+      counter,
+      numberOfQuestions,
+      categoryName,
+      question,
+      choices
+    } = this.props;
+
+    if (counter > numberOfQuestions) {
+      return <ResultDisplay total={numberOfQuestions} />;
+    }
+
+    return (
+      <div className="main container has-text-centered">
+        <CategoryName name={categoryName} />
+        <QuestionCounter counter={counter} total={numberOfQuestions} />
+        <Question question={question} />
+        <hr />
+        <ChoiceList choices={choices} />
+      </div>
+    );
+  }
 }
 
 GameBoard.propTypes = {
   counter: PropTypes.number.isRequired,
   numberOfQuestions: PropTypes.number.isRequired,
-  hasQuestionAnswered: PropTypes.bool.isRequired,
   categoryName: PropTypes.string.isRequired,
   question: PropTypes.string.isRequired,
-  choices: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired
+  choices: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  onSetIdNextQuetion: PropTypes.func.isRequired
 };
 
-export default connect(stateToProps)(GameBoard);
+export default connect(stateToProps, dispatchToProps)(GameBoard);
