3e563fbd0e033a64fedcbf3cc5cf61789930fd0c
Mohcine EL KASSIB <elkassib.mohcine@gmail.com>
2018-01-21 23:18:37 +0000

    Add github ribbon

--- client/components/layouts/Main/index.js
+++ client/components/layouts/Main/index.js
@@ -3,11 +3,27 @@ import React from 'react';
 import CategoryList from 'Components/components/CategoryList';
 import { selectCategories as categories } from 'Api/utils';
 
+import githubRibbon from 'Assets/github-ribbon.png';
+
 export default function Main() {
   return (
-    <main className="main container has-text-centered">
-      <h2 className="title is-2">Séléctionner une catégorie</h2>
-      <CategoryList categories={categories} />
-    </main>
+    <React.Fragment>
+      <a
+        href="https://github.com/elkamondo/elka-quiz"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="is-hidden-mobile"
+      >
+        <img
+          style={{ position: 'absolute', top: 0, right: 0, border: 0 }}
+          src={githubRibbon}
+          alt="Fork me on GitHub"
+        />
+      </a>
+      <main className="main container has-text-centered">
+        <h2 className="c-main-title title is-2">Séléctionner une catégorie</h2>
+        <CategoryList categories={categories} />
+      </main>
+    </React.Fragment>
   );
 }
